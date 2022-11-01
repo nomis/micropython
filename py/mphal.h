16c0f8d3548d2e287e7ebbc82457661fc76a628a
Simon Arlott <sa.me.uk>
2022-11-01 09:42:11 +0000

    Fix time Epoch on the ESP32

--- py/mphal.h
+++ py/mphal.h
@@ -75,9 +75,14 @@ mp_uint_t mp_hal_ticks_us(void);
 mp_uint_t mp_hal_ticks_cpu(void);
 #endif
 
+#ifndef mp_hal_time_s
+// Seconds since the Epoch.
+int32_t mp_hal_time_s(void);
+#endif
+
 #ifndef mp_hal_time_ns
 // Nanoseconds since the Epoch.
-uint64_t mp_hal_time_ns(void);
+int64_t mp_hal_time_ns(void);
 #endif
 
 // If port HAL didn't define its own pin API, use generic
